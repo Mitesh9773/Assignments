c07c2f04ca15d9eeb56606b34b6507ec032a485d
Rathod Mitesh <rathodmeet9153@gmail.com>
2025-07-22 23:24:38 +0530

    Add files via upload

---
+++ Module 6  Python Fundamentals/Practical/3. Core Python Concepts/typeChecks.py
@@ -0,0 +1,11 @@
+# Example 4: Checking Variable Types
+
+name = "Mitesh"
+age = 19
+height = 5.6
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
