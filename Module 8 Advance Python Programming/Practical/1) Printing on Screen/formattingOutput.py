fdccc38ca5185572b0d4a8e95d7025b460d6a4d1
Rathod Mitesh <rathodmeet9153@gmail.com>
2025-08-18 12:17:37 +0530

    Add files via upload

---
+++ Module 8 Advance Python Programming/Practical/1) Printing on Screen/formattingOutput.py
@@ -0,0 +1,13 @@
+# Q) Write a Python program to print a formatted string using print() and f-string.
+
+# formatted string
+# name = "Mitesh"
+# age = 20
+
+# print(f"My name is {name}. I am {age} years old.")
+
+# f-string
+name = "Mitesh"
+age = 20
+
+print("My name is {} and I am {} years old.".format(name,age))
---
+++ Module 8 Advance Python Programming/Practical/5) Exception Handling/fileException.py
@@ -0,0 +1,24 @@
+# Write a Python program to handle file exceptions and use the finally block for closing
+# the file
+
+try:
+    file_name = input("Enter the file name: ")
+    f = open(file_name,"r")
+    content = f.read()
+    print("File content:\n", content)
+
+except FileNotFoundError:
+    print("The file is not found.")
+
+except PermissionError:
+    print("You don't have permission to read this file.")
+
+except Exception as e:
+    print(f"Unexcepted error occours: {e}")
+
+finally:
+    try:
+        f.close()
+        print("File closed successfully.")
+    except NameError:
+        print("No file was opened.")
